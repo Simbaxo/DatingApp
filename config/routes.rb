46c7057c93d9b2d4d0bf3666a50ee4a3717a4182
Andres <ballerlikemike23@gmail.com>
2017-08-23 22:53:38 -0400

    Added footer, about, contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   resources :profiles
   get '/signup', to: 'profiles#new'
+  get '/about', to: 'pages#about'
+  get '/contact', to: 'pages#contact'
   get 'profiles/index'
   root 'pages#home'
 end
